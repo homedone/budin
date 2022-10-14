aa0b2537f7d3f2856082abea0147963d24536513
homedone-lab <duxq2021@126.com>
2022-10-14 22:39:02 +0800

    update vueproject budin-web

--- budin-file-service/src/main/java/indiv/budin/fileservice/controller/FileServiceController.java
+++ budin-file-service/src/main/java/indiv/budin/fileservice/controller/FileServiceController.java
@@ -1,23 +1,32 @@
 package indiv.budin.fileservice.controller;
 
 
+import indiv.budin.common.utils.ResultUtil;
 import io.minio.MinioClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 
 @Controller
 public class FileServiceController {
     @RequestMapping("/")
-    public String portal(){
+    public String portal() {
         return "index.html";
     }
-    @RequestMapping("/download")
-    public void download(@RequestParam("fileName") String fileName){
 
+    @RequestMapping("/api/download")
+    public ResultUtil<String> download(@RequestParam("fileName") String fileName) {
+        return ResultUtil.fail();
+    }
+
+    @RequestMapping("/api/upload")
+    public ResultUtil<String> upload(@RequestParam String fileName, @RequestParam MultipartFile multipartFile) {
+        //do something
+        return ResultUtil.fail();
     }
 
 }
--- budin-web/src/App.vue
+++ budin-web/src/App.vue
@@ -1,26 +1,15 @@
-/* eslint-disable */
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <budin/>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
+
 <script>
-import budin from './components/budin'
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
   name: 'App',
   components: {
-    budin
+    HelloWorld
   }
 }
 </script>
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
