ccfc8b692fa69c153dd5158401df3c67c485f10e
homedone-lab <duxq2021@126.com>
2022-11-12 22:39:22 +0800

    update irpc

---
+++ budin-rpc/src/main/java/indiv/budin/rpc/irpc/client/RpcClient.java
@@ -0,0 +1,16 @@
+package indiv.budin.rpc.irpc.client;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @author
+ * @date 2022/11/12 20 41
+ * discription
+ */
+public class RpcClient implements InvocationHandler {
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
